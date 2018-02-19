53ee5051908946fea2dbbd957e9f47ab9dd08f3d
Richard Burd <richardburdx@yahoo.com>
2018-02-19 16:10:27 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,8 +12,9 @@ class StudentsController < ApplicationController
 
   end
 
-  def create 
-
-  end 
+  def create
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to posts_path
+  end
 
 end
